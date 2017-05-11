5465a671553d40e1b73453bc5c49df63c86a5635
Bryan J. Agee <bryan@pamiris.com>
2017-05-10 17:19:49 -0700

    Adds env var for debug mode (rather than toggling a comment

--- library/HelloSign/Test/AbstractTest.php
+++ library/HelloSign/Test/AbstractTest.php
@@ -47,14 +47,14 @@ abstract class AbstractTest extends \PHPUnit_Framework_TestCase
         }
 
 
-        $api_url = $_ENV['API_URL'] == null ? Client::API_URL : $_ENV['API_URL'];
-        $oauth_token_url = $_ENV['OAUTH_TOKEN_URL'] == null ? Client::OAUTH_TOKEN_URL : $_ENV['OAUTH_TOKEN_URL'];
-        $this->client = new Client($_ENV['API_KEY'], null, $api_url, $oauth_token_url);
-        $this->team_member_1 = rand(1, 10000000) . "@example.com";
-        $this->team_member_2 = rand(1, 10000000) . "@example.com";
-        // $this->client->enableDebugMode();
-      
-        if ($api_url != Client::API_URL) {
+
+        // To enable debug mode, edit the env in phpunit.xml
+        if (!empty($_ENV['DEBUG_MODE'])) {
+            echo "Debug mode enabled\n";
+            $this->client->enableDebugMode();
+        }
+
+        if ($api_url !== Client::API_URL) {
             $this->client->disableCertificateCheck();
         }
         
